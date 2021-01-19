81b196331f8f5e877166a6f420d41e42dd010484
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-01-19 11:36:50 +0100

    Exo-138

--- index.php
+++ index.php
@@ -2,20 +2,22 @@
 
 // Premiere ligne
 
-{
+$helloWorld = function (){
     echo "Hello World!";
-}
+};
 
 //Deuxieme ligne
-
+$helloWorld();
 
 
 //Troisieme ligne
 
-function maFonctionParam($fname, $lname) {
-    echo ;
+function maFonctionParam ($fname, $lname) {
+    echo $fname ;
+    return ($lname);
 }
 
+maFonctionParam("Bonjour", 'Hello');
 
 
 
